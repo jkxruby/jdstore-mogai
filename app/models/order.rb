5c20029aea396d5457256a6004f09f3db278ea30
jkxruby <jkxruby@gmail.com>
2017-07-11 17:15:40 +0800

    add payment_method function

--- app/models/order.rb
+++ app/models/order.rb
@@ -11,6 +11,14 @@ validates :shipping_address, presence: true
 
 def generate_token
   self.token = SecureRandom.uuid
+end
+
+def set_payment_with!(method)
+  self.update_columns(payment_method: method)
+end
+
+def pay!
+  self.update_columns(is_paid: true)
 end 
 
 end
