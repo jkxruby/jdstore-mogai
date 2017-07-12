3adc8e85e046b4b8ce7ad257fa9cee4ae8b3a783
jkxruby <jkxruby@gmail.com>
2017-07-12 10:19:32 +0800

    implement admin/orders show function

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,6 +5,11 @@ before_action :admin_required
 
 def index
   @orders = Order.order("id DESC")
+end
+
+def show
+  @order = Order.find(params[:id])
+  @product_lists = @order.product_lists
 end 
 
 end
