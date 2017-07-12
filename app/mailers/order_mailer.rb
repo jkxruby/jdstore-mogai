498da5267e53c7513824534a01e6bef61d1fe609
jkxruby <jkxruby@gmail.com>
2017-07-12 12:02:13 +0800

    add apply_cancel function

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -5,5 +5,14 @@ class OrderMailer < ApplicationMailer
     @product_lists = @order.product_lists
 
     mail(to: @user.email, subject: "[老木匠]感谢您完成本次下单，以下是您的购物明细#{order.token}")
+  end
+
+  def apply_cancel(order)
+    @order = order
+    @user = order.user
+    @product_lists = @order.product_lists
+
+    mail(to: "admin@test.com", subject: "[老木匠]用户#{order.user.email}申请取消订单#{order.token}")
   end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ namespace :admin do
       post :shipped
       post :return
     end
-  end 
+  end
 end
 
 resources :products do
@@ -35,6 +35,7 @@ resources :orders do
   member do
     post :pay_with_alipay
     post :pay_with_wechat
+    post :apply_to_cancel
   end
 end
 
