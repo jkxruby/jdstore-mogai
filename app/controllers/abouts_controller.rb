ef0d9e9d06dc1a8c359f09225f44a1a8bfaef759
jkxruby <jkxruby@gmail.com>
2017-07-21 16:03:18 +0800

    finish the aout.html.erb

---
+++ app/controllers/abouts_controller.rb
@@ -0,0 +1,4 @@
+class AboutsController < ApplicationController
+  def about
+  end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
+get 'abouts/about'
 
 namespace :admin do
   resources :products
@@ -19,7 +19,7 @@ end
 resources :products do
   collection do
     get :search
-  end 
+  end
   member  do
     post :add_to_cart
   end
@@ -46,6 +46,7 @@ namespace :account do
   resources :users
   resources :orders
 end
+
 
 root 'welcome#index'
 
