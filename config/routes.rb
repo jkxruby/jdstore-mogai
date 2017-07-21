50bac4aaabc3647397f5a490f0411bccf9ff8c35
jkxruby <jkxruby@gmail.com>
2017-07-21 17:27:12 +0800

    modify some detail

--- config/routes.rb
+++ config/routes.rb
@@ -2,52 +2,50 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-get 'abouts/about'
+  get 'abouts/about'
 
-namespace :admin do
-  resources :products
-  resources :orders do
-    member do
-      post :cancel
-      post :ship
-      post :shipped
-      post :return
+  namespace :admin do
+    resources :products
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
     end
   end
-end
 
-resources :products do
-  collection do
-    get :search
+  resources :products do
+    collection do
+      get :search
+    end
+    member do
+      post :add_to_cart
+    end
   end
-  member  do
-    post :add_to_cart
+
+  resources :carts do
+    collection do
+      delete :clean
+      post :checkout
+    end
   end
-end
 
-resources :carts do
-  collection do
-    delete :clean
-    post :checkout
+  resources :cart_items
+
+  resources :orders do
+    member do
+      post :pay_with_alipay
+      post :pay_with_wechat
+      post :apply_to_cancel
+    end
   end
-end
 
-resources :cart_items
-
-resources :orders do
-  member do
-    post :pay_with_alipay
-    post :pay_with_wechat
-    post :apply_to_cancel
+  namespace :account do
+    resources :users
+    resources :orders
   end
-end
 
-namespace :account do
-  resources :users
-  resources :orders
-end
-
-
-root 'welcome#index'
-
+  root 'welcome#index'
 end
